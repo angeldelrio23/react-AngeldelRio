e9940038a3266766fa07b8bff24c13fbd8cec1b2
angeldelrio23 <angeldelrio23@yahoo.es>
2022-01-26 13:33:42 +0100

    Usuarios.js creado con los datos

---
+++ src/data/Usuarios.js
@@ -0,0 +1,34 @@
+import Home from '../components/Home';
+import { PaginaListas } from '../components/PaginaListas';
+import { Perfil } from '../components/Perfil';
+
+export const Usuarios = [
+  {
+    id: 1,
+    usuario: 'Angeldelrio23',
+    contraseña: 'angel',
+    nombre: 'Angel del Rio',
+    correo: 'Angeldelrio23@gmail.com',
+  },
+  {
+    id: 2,
+    usuario: 'Shurklex',
+    contraseña: 'labrum',
+    nombre: 'Alejandro Fernandez',
+    correo: 'Shurklex@gmail.com',
+  },
+  {
+    id: 3,
+    usuario: 'MaxPrain',
+    contraseña: 'aglinformatica',
+    nombre: 'Alder Velasquez',
+    correo: 'MaxPrain@gmail.com',
+  },
+  {
+    id: 4,
+    usuario: 'Phantom432',
+    contraseña: 'vexIRL',
+    nombre: 'Pablo Garcia',
+    correo: 'Phantom432@gmail.com',
+  },
+];
